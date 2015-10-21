e9597e16810d44a61170a0e1ecdc49a773f92d72
Jesse Zelaya <jessezelaya1@gmail.com>
2015-10-21 10:48:08 -0700

    Set up factory to get next group of people

--- app.js
+++ app.js
@@ -15,11 +15,19 @@
     // ==================================================
     function swFactory($http) {
       console.log('factory');
-      var allPeopleUrl = 'http://swapi.co/api/people/?format=json'
+      var allPeopleUrl = "http://swapi.co/api/people/?page=1&format=json"
       var swFactory = {};
 
       swFactory.getAllPeople = function(){
         return $http.get(allPeopleUrl);
+      }
+
+      swFactory.getPreviousPeopleGroup = function(prev){
+        return $http.get(prev);
+      }
+
+      swFactory.getNextPeopleGroup = function(next){
+        return $http.get(next);
       }
 
       return swFactory;
@@ -33,13 +41,25 @@
     function swController(SwFactory) {
       var vm = this;
       vm.api = SwFactory;
+      vm.data = []
       vm.people = [];
 
       vm.api.getAllPeople()
         .success(function(data){
-          console.log(data);
+          vm.data = data;
           vm.people = data.results;
+          console.log(vm.data);
+          console.log(vm.people);
         })
+
+      vm.showNextPeople = function(next){
+        vm.api.getNextPeopleGroup(next)
+          .success(function(data){
+            vm.data = data
+            vm.people = data.results
+            console.log(data);
+          })
+      }
 
     }
 
